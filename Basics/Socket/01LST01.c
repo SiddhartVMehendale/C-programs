296e794cea31fe8688f1ea35a1c17f9ebc1b9024
SiddhartVMehendale <mehendalesiddharth1997@gmail.com>
2020-06-13 17:47:14 +0530

    This is the first commmit

---
+++ Basics/Socket/01LST01.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+
+int main(int argc, char **argv){
+
+    int z;                                              // Status return code
+    int s[2];                                           // Pair of Sockets
+
+    /*  Creating a pair of local sockets*/
+
+
+    z = socketpair(AF_LOCAL, SOCK_STREAM, 0 ,s);
+
+
+    if(z == -1){
+        fprintf(stderr, "%s: socketpair(AF_LOCAL, SOCK_STREAM, 0 ) \n", strerror(errno));
+        return 1;
+    }
+
+    /*  Report the socket file descriptors returned*/
+
+    printf("s[0] = %d; \n",s[0]);
+    printf("s[1] = %d;\n",s[1]);
+
+    system("netstat --unix -p");
+
+    return 0;
+}
