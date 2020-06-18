2cdf288a823f489267aa327f5a2a846e9feec993
SiddhartVMehendale <mehendalesiddharth1997@gmail.com>
2020-06-18 10:21:48 +0530

    This is the final bubble sort program

--- Basics/Data_Structures/Sorting/bubble/bubble.c
+++ Basics/Data_Structures/Sorting/bubble/bubble.c
@@ -2,9 +2,10 @@
 
 void accept(int *);
 void display(int *);
+void bubble(int *);
 int n;
 
-void display(int *array)
+void display(int *array)                                                     // Reusable function to display the array
 {
     int i;
     for(i = 0 ; i < n ; i++)
@@ -16,7 +17,7 @@ void display(int *array)
     printf("\n");
 }
 
-void accept(int *array)
+void accept(int *array)                                                       // Function to initially accept the array
 {
     int i;
     printf("\n How many elements do you want in the array: ");
@@ -29,12 +30,40 @@ void accept(int *array)
     }
 }
 
+void bubble(int *array)
+{
+    int hold, iter, pass, number_of_passes = 0;
+    int switched = 1;
+
+    for( pass = 0 ; pass < n-1 && switched == 1 ; pass++)
+    {
+        switched = 0;
+        for( iter = 0 ; iter < n - pass - 1 ; iter++)
+        {
+            if(array[iter] > array[iter+1])
+            {
+                switched = 1;
+                hold = array[iter];
+                array[iter] = array[iter+1];
+                array[iter+1] = hold;
+            }
+        }
+        number_of_passes++;
+    }
+
+    printf("\n The number of passes are: %d", number_of_passes);
+}
+
 int main()
 {
     int array[100];
 
     accept(array);
     printf("\n Array before sorting: ");
+    display(array);
+
+    bubble(array);
+    printf("\n The array after sorting\n");
     display(array);
     return 0;
 }
