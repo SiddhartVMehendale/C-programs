27c649afe9891697a4decbc942f9b331c56e8ebb
SiddhartVMehendale <mehendalesiddharth1997@gmail.com>
2020-07-28 12:55:26 +0530

    Added test for stack using linked list

--- Basics/Data_Structures/stack/test/StackUsingLinkedList_test.cpp
+++ Basics/Data_Structures/stack/test/StackUsingLinkedList_test.cpp
@@ -11,9 +11,48 @@ TEST(push, default_data_true)
     bool result = false;
 
     result = push(&top,data);
+    ASSERT_TRUE(result);
+}
+
+TEST(pop, default_data_true)
+{
+    struct stack *top;
+    top = NULL;
+    int data = 4;
+    bool result = false;
+
+    push(&top, data);
+
+    result = pop(&top, &data);
+    printf("\n%d\n", data);
 
     ASSERT_TRUE(result);
 }
+
+TEST(pop, defult_data_false)
+{
+    struct stack *top;
+    top = NULL;
+    int data;
+    bool result = false;
+    printf("\nAssert for false\n");
+    result = pop(&top, &data);
+    ASSERT_FALSE(result);
+}
+
+TEST(Push_For_Data_Null, default_data_false)
+{
+    struct stack *top;
+    top = NULL;
+    int data = NULL;
+    bool result = true;
+    printf("\n Test for data = NULL\n");
+    result = push(&top, data);
+
+    ASSERT_FALSE(result);
+}
+
+
 
 int main(int argc, char **argv)
 {
